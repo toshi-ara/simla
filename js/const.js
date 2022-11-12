f3acf92ed234833e3695a7522a4a23329a08c4ac
ARA Toshiaki (Univ.) <toshiaki.ara@gmail.com>
2022-11-12 10:23:16 +0900

    divide const*.js files

--- js/const.js
+++ js/const.js
@@ -20,18 +20,3 @@ const ProbThreshold = 0.05
 // name of local strage
 const storageName = "simlationLocalAnesthesia"
 
-
-// labels
-const
-    label_start = "Start",
-    label_restart = "Restart",
-    label_pause = "Pause",
-    label_newexp = "New Exp.",
-    label_quit = "Quit",
-    label_with_response = "Respond",
-    label_without_response = "Not Respond",
-    label_speed = "x speed";
-const
-    msg_newexp = "Do you want to start a new experiment?",
-    msg_quit = "Do you want to quit?",
-    msg_close = "Please close the browser or this tab";
---
+++ js/label_ja.js
@@ -0,0 +1,13 @@
+const
+    label_start = "開始",
+    label_restart = "再開",
+    label_pause = "一時停止",
+    label_newexp = "新規実験",
+    label_quit = "終了",
+    label_with_response = "反応あり",
+    label_without_response = "反応なし",
+    label_speed = "倍速";
+const
+    msg_newexp = "新規実験を行いますか?",
+    msg_quit = "終了しますか?",
+    msg_close = "ブラウザあるいはこのタブを閉じて下さい";
