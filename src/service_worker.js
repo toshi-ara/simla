fc5c1398f4d83865a16ab6ebf11c19fdedf60029
ARA Toshiaki (Univ.) <toshiaki.ara@gmail.com>
2022-12-07 08:13:16 +0900

    upadate

--- src/service_worker.js
+++ src/service_worker.js
@@ -1,25 +1,15 @@
-const cacheName = 'simla-caches';
-const cacheFiles = [
-    'index.html'
-];
-
+const CACHE_NAME = 'simla-caches';
+const urlsToCache = ['index.html'];
 
 self.addEventListener('install', event => {
-    caches
-        .open(cacheName).
-        then(cache => cache.addAll(cacheFiles));
+    event.waitUntil(
+        caches
+            .open(CACHE_NAME)
+            .then(cache => {
+                return cache.addAll(urlsToCache.map);
+            })
+    );
 });
-
-// self.addEventListener('install', event => {
-//     event.waitUntil(
-//         caches
-//             .open(CACHE_NAME)
-//             .then(cache => {
-//                 return cache.addAll(urlsToCache.map);
-//                 // return cache.addAll(urlsToCache.map(url => new Request(url, {credentials: 'same-origin'})));
-//             })
-//     );
-// });
 
 self.addEventListener('fetch', event =>  {
     event.respondWith(
